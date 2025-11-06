e17914ac0c4c7100631f95ac2ba957c37d54a1ba
StefanKleemann <stefan.kleemann@hhu.de>
2025-11-06 09:07:54 +0100

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,19 @@
+## Download soll sein von
+
+import requests
+
+fileserver = "https://www.math.uni-duesseldorf.de/~zibrowius/TeachingRepo/"
+
+
+## Diese Methode zeigt den Filetree des gegebenen resource an
+def getIndex(resource):
+
+    return 0
+
+def download():
+    return 0
+
+getIndex()
+download()
+
+
